a2fdda3a32f789f1f7dfd99ddbcc9e08659e3e26
Matthew <unsolvedcypher@gmail.com>
2014-10-17 01:15:52 -0400

    Added the solve function, finished cosine functions. The program should be all done (but is untested).

--- Triangle-Solver.cpp
+++ Triangle-Solver.cpp
@@ -11,17 +11,26 @@
 #include <cmath>
 using namespace std;
 
-void Process(double triangle [6]);
 /* executes functions in order */
-bool IsInvalid(double triangle [6]);
+void Process(double triangle [6]); /// The name and description of this
+                                   /// function is very vague. I think it may
+                                   /// be best to split the fuction up if it
+                                   /// doesn't have one clear purpose.
+/* runs a loop to solve the whole triangle */
+void SolveTriangle(double triangle[6]);
 /* checks to see if final triangle is valid */
-void Input(double triangle[6]);
+bool IsInvalid(double triangle [6]);
 /* take user input of known angles and sides */
+void Input(double triangle[6]);
 
-void SolveMissingSideLOS(double triangle[6], int missing_side);
 /* solve missing side using law of sines */
-void SolveMissingAngleLOS(double triangle[6], int missing_angle);
+void SolveMissingSideLOS(double triangle[6], int missing_side);
 /* solve missing angle using law of sines */
+void SolveMissingAngleLOS(double triangle[6], int missing_angle);
+/* solve missing side using law of cosines */
+void SolveMissingSideLOC(double triangle[6], int missing_side);
+/* solve missing angle using law of cosines */
+void SolveMissingAngleLOC(double triangle[6], int missing_angle);
 
 string GetTriangleType(double triangle[6]);
 /* returns the type of triangle (ie. "equilateral") */
@@ -31,12 +40,12 @@ void SolveLastAngle(double &angle1, double &angle2, double &angle3);
 /* takes in 3 angles, and solves the missing one */
 
 
-double GetAngleToSideRatio(double triangle[6]);
 /* figures out sinx/x ratio for law of sines */
-double GetSideToAngleRatio(double triangle[6]);
+double GetAngleToSideRatio(double triangle[6]);
 /* figures out x/sinx ratio for law of sines */
-void SolveLastAngle(double &angle1, double &angle2, double &angle3);
+double GetSideToAngleRatio(double triangle[6]);
 /* solves last angle based on the fact that all angles add up to 180 */
+void SolveLastAngle(double &angle1, double &angle2, double &angle3);
 
 int main() {
     double triangle[6];
@@ -155,13 +164,11 @@ string GetAngleType(double triangle[6]) {
 }
 
 void SolveLastAngle(double &angle1, double &angle2, double &angle3) {
-  if (angle1 && angle2) {
-    angle3 = 180 - (angle1 + angle2);
-  } else if (angle1 && angle3) {
-    angle2 = 180 - (angle1 + angle3);
-  } else if (angle2 && angle3) {
-    angle1 = 180 - (angle2 + angle3);
-  }
+  /// wouldn't it just be easier to pass the triangle and the unsolved
+  /// angle number?
+  /* this function assumes angle 3 needs to be solved,
+     so pass angles accordingly */
+  angle3 = 180 - (angle1 + angle2);
 }
 
 double GetSideToAngleRatio(double triangle[6]) {
@@ -191,4 +198,52 @@ void SolveMissingAngleLOS(double triangle[6], int missing_angle) {
   triangle[missing_angle] =
   asin(GetAngleToSideRatio(triangle) * triangle[(missing_angle + 3) % 6]);
 }
+void SolveMissingSideLOC(double triangle[6], int missing_side) {
+  triangle[missing_side] = sqrt(
+  pow(triangle[(missing_side + 2) % 6], 2) + /* a^2 */
+  pow(triangle[(missing_side - 2) % 6], 2) - /* b^2 */
+  /* 2ab*cosC */
+  (2 * triangle[(missing_side + 2) % 6] * triangle[(missing_side - 2) % 6] *
+   cos(triangle[(missing_side + 3) % 6])));
+}
 
+void SolveMissingAngleLOC(double triangle[6], int missing_angle) {
+  triangle[missing_angle] =
+  acos(
+  /* a^2 + b^2 - c^2 */
+  (pow(triangle[(missing_angle + 1) % 6], 2) +
+   pow(triangle[(missing_angle + 5) % 6], 2) -
+   pow(triangle[(missing_angle + 3) % 6], 2)) /
+  /* 2ab */
+  (2 * triangle[(missing_angle + 1) % 6] * triangle[(missing_angle + 5) % 6]));
+}
+
+void SolveTriangle(double triangle[6]) {
+  for(int i = 0; i < 6; i += 2) {
+    /* if 2 angles are solved but angle i is not */
+    if (!triangle[i] && triangle[(i + 2) % 6] && triangle[(i - 2) % 6]) {
+      SolveLastAngle(triangle[(i + 2) % 6], triangle[(i - 2) % 6], triangle[i]);
+      i = 0;
+      /* if all sides are known, but angle i isn't */
+    } else if (triangle[1] && triangle[3] && triangle[5] && !triangle[i]) {
+      SolveMissingAngleLOC(triangle, i);
+      i = 0;
+    } else if (triangle[(i - 1) % 6] && triangle[(i - 2) % 6] &&
+               triangle[(i - 3) % 6] && !triangle[(i + 1) % 6]) {
+      SolveMissingSideLOC(triangle, (i + 1));
+      i = 0;
+    } else if (((triangle[i + 1] && triangle[(i + 4) % 6]) ||
+                (triangle[(i - 1) % 6] && triangle[(i - 4) % 6])) &&
+               !triangle[i]) {
+      SolveMissingAngleLOS(triangle, i);
+      i = 0;
+    } else if (((triangle[(i + 2) % 6] && triangle[(i + 5) % 6]) ||
+                (triangle[i] && triangle[(i - 3) % 6])) &&
+               !triangle[i + 1]) {
+      SolveMissingSideLOS(triangle, (i + 1));
+      i = 0;
+    }
+  }
+  cout << "all done\n\n";
+  cin.get();
+}
